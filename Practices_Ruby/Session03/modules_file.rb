174b9af92071546be2186c3535bd0bb7f300e714
oscarDelgadilloT <oscar.delgadillot@jalasoft.com>
2018-03-06 15:12:59 -0400

    Solved modules practice

---
+++ Practices_Ruby/Session03/modules_file.rb
@@ -0,0 +1,35 @@
+# 1. Write a module that print different type of greets (Hi Friend, good night, good afternoon, etc )
+
+module Greeters
+  def say_hi
+    puts "Hi my friend...!!"
+  end
+  def nigth
+    puts "Good night..!!"
+  end
+  def afternoon
+    puts "Good afternoon..!!"
+  end
+end
+
+# Greets.say_hi
+# Greets.nigth
+# Greets.afternoon
+
+# 2. Write another module that print different ways to say bye (good bye, see you later, take care, etc)
+
+module Bye
+  def say_bye
+    puts "Goog bye my friend...!!"
+  end
+  def later
+    puts "See you later..!!"
+  end
+  def take_care
+    puts "Take care my friend..!!"
+  end
+end
+
+# Bye.say_bye
+# Bye.later
+# Bye.take_care
