6b5eb86dbe7a74dd5a96f6c8c6f7013b3cbdca7b
Oscar Delgadillo <oscar.delgadillo@fundacion-jala.org>
2018-03-08 00:45:47 -0400

    Solved control statements practice

--- Practices_Ruby/Session04/control_statements2.rb
+++ Practices_Ruby/Session04/control_statements2.rb
@@ -6,7 +6,7 @@
 # 3. Method to add passengers to a flight. If the flight is full return a message that should pickup another flight.
 # For this cases you should use hashes to save the data and also variables to keep the values.
 
-class Client
+class Passenger
 
   def initialize(name, last_name, address, phone, years)
     @name = name
@@ -16,12 +16,24 @@ class Client
     @years = years
     @type = years < 18 ? "Child" : "Adult"
   end
+
+  def get_full_name
+    @name + " " + @last_name
+  end
+
 end
 
 class Flight
 
+  attr_reader :itinerary
+  attr_reader :passengers
+
+  def initialize
+    @itinerary = {}
+    @passengers = {}
+  end
+
   def load_itinerary
-    itinerary = {}
     print "Insert origin: "
     origin = gets.chomp
     print "Insert destination: "
@@ -29,39 +41,33 @@ class Flight
     print "Insert number of flight: "
     number_flight = gets.chomp
     print "Insert number of passengers: "
-    number_passengers = gets.chomp
-    itinerary.store "Origin", origin
-    itinerary.store "Destination", destination
-    itinerary.store "Number_Flight", number_flight
-    itinerary.store "Number_Passengers", number_passengers
-    return itinerary
+    number_passengers = gets.chomp.to_i
+    @itinerary.store "Origin", origin
+    @itinerary.store "Destination", destination
+    @itinerary.store "Number_Flight", number_flight
+    @itinerary.store "Number_Passengers", number_passengers
   end
 
-  def add_passengers(itinerary, people)
-    i = 0
-    passengers = {}
-    while i < itinerary['Number_Passengers'].to_i do
-      passengers.store i, people[i]
-      i += 1
+  def add_passenger(passenger)
+    if @passengers.size < @itinerary['Number_Passengers']
+      @passengers.store @passengers.size + 1, passenger
+    else
+      puts "The flight is full. #{passenger.get_full_name} you should pickup another flight."
     end
-    # if i < itinerary['Number_Passengers'].to_i
-    if i > itinerary['Number_Passengers'].to_i
-      puts "Flight is full"
-    end
-    puts "Passengers on Flight: #{passengers}"
-    puts "Limit of Passengers: #{itinerary['Number_Passengers']}"
   end
 end
 
-client1 = Client.new("Homero", "Simpsons", "xyz", 123465, 40)
-client2 = Client.new("March", "Simpsons", "xyz", 123465, 30)
-client3 = Client.new("Bart", "Simpsons", "xyz", 123465, 12)
-client4 = Client.new("Lisa", "Simpsons", "xyz", 123465, 10)
-passengers = []
-passengers.push(client1)
-passengers.push(client2)
-passengers.push(client3)
-passengers.push(client4)
-flightObject = Flight.new
-itinerary = flightObject.load_itinerary
-flightObject.add_passengers(itinerary, passengers)
+p1 = Passenger.new("Homero", "Simpsons", "xyz", 123465, 40)
+p2 = Passenger.new("March", "Simpsons", "xyz", 123465, 30)
+p3 = Passenger.new("Bart", "Simpsons", "xyz", 123465, 12)
+p4 = Passenger.new("Lisa", "Simpsons", "xyz", 123465, 10)
+
+flight = Flight.new
+flight.load_itinerary
+flight.add_passenger(p1)
+flight.add_passenger(p2)
+flight.add_passenger(p3)
+flight.add_passenger(p4)
+
+p flight.itinerary
+p flight.passengers
